3d0d69a37276234f2d65ac983ba3e5161c0115de
Donekulda <mikulda@hotmail.cz>
2019-10-17 21:06:39 +0200

    upload
    3/4 hotová knihovna

--- Library/src/school/absInt/Diary.java
+++ Library/src/school/absInt/Diary.java
@@ -1,4 +1,17 @@
 package school.absInt;
 
 public class Diary extends Template implements Repair {
+    private String name;
+    private String author;
+    private int pages;
+    private int durability;
+    private boolean lended;
+
+    public Diary(String name, String author, int pages, int durability) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.durability = durability;
+        this.lended = false;
+    }
 }
--- Library/src/school/absInt/Library.java
+++ Library/src/school/absInt/Library.java
@@ -11,13 +11,39 @@ public class Library implements Lend{
     List<Magasine> invOfMagasines = new ArrayList<Magasine>();
     List<Diary> invOfDiaries = new ArrayList<Diary>();
 
-    // 1. Kniha
+    private String nameOfLibrary;
+
+    public Library(String nameOfLibrary) {
+        this.nameOfLibrary = nameOfLibrary;
+        System.out.println("Byla vytvořena knihovna " + nameOfLibrary);
+        System.out.println("===============================================================");
+    }
+
+    @Override
+    public String toString() {
+        return "\n===============================================================" +
+                "Knihovna " + nameOfLibrary +
+                "\nPočet knih: " + invOfBooks +
+                "\nPočet časopisů: " + invOfMagasines +
+                "\nPočet Diářu: " + invOfDiaries +
+                "\n===============================================================";
+    }
+
+    // 1. Book
     // 2. Magasine
     // 3. Diary
     public void addItem(int Type){
         switch (Type){
             case 1:
+                addBook();
+                break;
 
+            case 2:
+                addMagasine();
+                break;
+
+            case 3:
+                addDiary();
                 break;
         }
     }
@@ -30,9 +56,109 @@ public class Library implements Lend{
         int year;
         Scanner input = new Scanner(System.in);
 
+        System.out.println("/n===============================================================");
+
         System.out.println("Zadejte název knihy: ");
         name = input.next();
 
+        System.out.println("Zadejte jméno autora: ");
+        author = input.next();
+
+        System.out.println("Zadejte rok vydání: ");
+        year = input.nextInt();
+
+        System.out.println("Zadejte počet stran: ");
+        pages = input.nextInt();
+
+        System.out.println("Zadejte v jakém stavu je kniha: ");
+        durability = input.nextInt();
+        System.out.print("%");
+
+        Book kniha = new Book(name, author, pages, durability, year);
+        invOfBooks.add(kniha);
+
+        System.out.println("===============================================================/n");
+        System.out.println("Byla přidána kniha " + name);
+        System.out.println("Autor: " + author);
+        System.out.println("Rok: " + year);
+        System.out.println("Počet stran: " + pages);
+        System.out.println("Stav(v %): " + durability);
+        System.out.println("/n===============================================================");
     }
 
+    private void addMagasine(){
+        String name;
+        String author;
+        int cislo;
+        int pages;
+        int durability;
+        int year;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("/n===============================================================");
+
+        System.out.println("Zadejte název časopisu: ");
+        name = input.next();
+
+        System.out.println("Zadejte jméno Nakladatel: ");
+        author = input.next();
+
+        System.out.println("Zadejte číslo časopisu: ");
+        cislo = input.nextInt();
+
+        System.out.println("Zadejte rok vydání: ");
+        year = input.nextInt();
+
+        System.out.println("Zadejte počet stran: ");
+        pages = input.nextInt();
+
+        System.out.println("Zadejte v jakém stavu je časopis: ");
+        durability = input.nextInt();
+        System.out.print("%");
+
+        Magasine casopis = new Magasine(name, author, cislo, pages, durability, year);
+        invOfMagasines.add(casopis);
+
+        System.out.println("===============================================================/n");
+        System.out.println("Byl přidán časopis " + name);
+        System.out.print(" č. " + cislo);
+        System.out.println("Nakldatelství: " + author);
+        System.out.println("Rok: " + year);
+        System.out.println("Počet stran: " + pages);
+        System.out.println("Stav(v %): " + durability);
+        System.out.println("/n===============================================================");
+    }
+
+    private void addDiary(){
+        String name;
+        String author;
+        int pages;
+        int durability;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("/n===============================================================");
+
+        System.out.println("Zadejte název diáře: ");
+        name = input.next();
+
+        System.out.println("Zadejte jméno autora: ");
+        author = input.next();
+
+        System.out.println("Zadejte počet stran: ");
+        pages = input.nextInt();
+
+        System.out.println("Zadejte v jakém stavu je diář: ");
+        durability = input.nextInt();
+        System.out.print("%");
+
+        Diary diar = new Diary(name, author, pages, durability);
+        invOfDiaries.add(diar);
+
+        System.out.println("===============================================================/n");
+        System.out.println("Byl přidán diář " + name);
+        System.out.println("Autor: " + author);
+        System.out.println("Počet stran: " + pages);
+        System.out.println("Stav(v %): " + durability);
+        System.out.println("/n===============================================================");
+    }
 }
