c8a9c870b3db23a143c37ec7b099f1f1b2f998a5
Donekulda <mikulda@hotmail.cz>
2019-10-17 20:33:02 +0200

    vylepšená verze

--- Library/src/school/absInt/Book.java
+++ Library/src/school/absInt/Book.java
@@ -1,4 +1,20 @@
 package school.absInt;
 
 public class Book extends Template implements Repair {
+
+    private String name;
+    private String author;
+    private int pages;
+    private int durability;
+    private int year;
+    private boolean lended;
+
+    public Book(String name, String author, int pages, int durability, int year) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.durability = durability;
+        this.year = year;
+        this.lended = false; // Bude vždy false ,protože když přidáme knížku tak ještě není přece půjčena
+    }
 }
--- Library/src/school/absInt/Library.java
+++ Library/src/school/absInt/Library.java
@@ -1,5 +1,38 @@
 package school.absInt;
 
 
-public class Library implements  Lend{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Library implements Lend{
+
+    List<Book> invOfBooks = new ArrayList<Book>();
+    List<Magasine> invOfMagasines = new ArrayList<Magasine>();
+    List<Diary> invOfDiaries = new ArrayList<Diary>();
+
+    // 1. Kniha
+    // 2. Magasine
+    // 3. Diary
+    public void addItem(int Type){
+        switch (Type){
+            case 1:
+
+                break;
+        }
+    }
+
+    private void addBook(){
+        String name;
+        String author;
+        int pages;
+        int durability;
+        int year;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Zadejte název knihy: ");
+        name = input.next();
+
+    }
+
 }
