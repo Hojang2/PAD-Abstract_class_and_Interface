c381b8f1c4e0c6b3bdb6764bfaf1834fa4cdbefa
Jan Buchmaier <janbuchmaier@seznam.cz>
2019-10-25 08:39:42 +0200

    Added fxml file

--- Library/src/school/absInt/GUI.java
+++ Library/src/school/absInt/GUI.java
@@ -1,11 +1,10 @@
 package school.absInt;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
@@ -14,21 +13,13 @@ public class GUI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        Scene scene = new Scene(root);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
-}
+        stage.setScene(scene);
+        stage.show();
+
+
+}}
