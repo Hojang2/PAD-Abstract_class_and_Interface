825ce9b8dbe178431c647fa7cf32da95c8ecff78
Donekulda <mikulda@hotmail.cz>
2019-12-13 08:19:37 +0100

    update
    nějaký bug

--- Library/src/school/absInt/UI/InventoryController.java
+++ Library/src/school/absInt/UI/InventoryController.java
@@ -3,10 +3,12 @@ package school.absInt.UI;
 import javafx.application.Application;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.event.*;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,10 +18,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import school.absInt.Book;
 import school.absInt.Library;
 import school.absInt.Template;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class InventoryController {
 
@@ -56,6 +60,14 @@ public class InventoryController {
         columnPages.setCellValueFactory(new PropertyValueFactory<>("Pages"));
         columnISBN.setCellValueFactory(new PropertyValueFactory<>("ISBN"));
         columnDurability.setCellValueFactory(new PropertyValueFactory<>("Durability"));
+/*
+        Book b = new Book("Hobit", "J.R.R. Tolkien", 300, "978-80-257-1949-9", 40);
+
+        lib.addPublication(b);
+
+        fillTable(10, 1);
+
+ */
     }
 
     @FXML
@@ -74,8 +86,21 @@ public class InventoryController {
         }
     }
 
-    private ObservableList<Template> fillTable(int itemsOnPage, int page){
+    private void fillTable(int itemsOnPage, int page){
+        ArrayList<Template> depositeArrayList = lib.getDeposite();
 
+        ArrayList<Template> depositeArrayListEdited = new ArrayList<Template>();
+
+        for(int i = (0+itemsOnPage*(page-1)); i < (itemsOnPage*page); i++){
+            if(i<depositeArrayList.size()){
+                depositeArrayListEdited.add(depositeArrayList.get(i));
+            }else{
+                break;
+            }
+        }
+
+        ObservableList<Template> depositList = FXCollections.observableArrayList(depositeArrayListEdited);
+        InventoryTable.setItems(depositList);
     }
 
     @FXML
